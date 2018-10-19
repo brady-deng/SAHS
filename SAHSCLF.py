6bf6e4dca5bb37c32455bfc4fe2de1c25966d7e8
brady-deng <3013506862@qq.com>
2018-10-19 10:55:12 +0800

    101912desk

--- SAHSCLF.py
+++ SAHSCLF.py
@@ -77,10 +77,10 @@ if __name__ == '__main__':
 
     # 单分类器训练与事件检测
     N = int(input('Please input the index of the subjects you want to train:'))
-    data,label = function.load_data('f10-23-1018.xlsx', N)
-    ind = np.array([ 1,2,7,9,11,13,14,21])
-    data = data[ind]
-    label = label[ind]
+    data,label = function.load_data('f60-12-ob.xlsx', N)
+    # ind = np.array([ 1,2,7,9,11,13,14,21])
+    # data = data[ind]
+    # label = label[ind]
     # data2 = data2[ind]
     # label2 = label2[ind]
     N = len(data)
