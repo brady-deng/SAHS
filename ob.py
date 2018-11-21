7f1d810d24f41b70d0848a5a4d677c45b20fd85e
brady-deng <3013506862@qq.com>
2018-11-21 10:15:35 +0800

    112110desk

---
+++ ob.py
@@ -0,0 +1,10 @@
+"""
+__title__ = ''
+__author__ = 'WNI10'
+__mtime__ = '2018/11/20'
+"""
+l = 2000
+for a in range(l):
+    for b in range(l):
+        for c in range(l):
+            print(a*l*l+b*l+c+1)
